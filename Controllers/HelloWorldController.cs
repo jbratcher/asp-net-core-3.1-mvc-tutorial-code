1cb9c5534835d70239d94ff53de7e93cb69a6dd8
jbratcher <jeremybratcher@gmail.com>
2020-02-11 15:09:26 -0500

    initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name = "Jeremy", int numTimes = 1)
+        {
+            ViewData["Message"] = $"Hi {name}";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+﻿# MvcMovie ASP.NET Core 3.1 MVC Tutorial Code
+
+This is the completed code for the current ASP.NET Core 3.1 MVC Tutorial from Microsoft.
+
+[Tutorial Link](https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/?view=aspnetcore-3.1)
+
+This repository serves as the complete code for Microsoft's ASP.NET Core 3.1  MVC tutorial as of 2/11/19.  The code base can serve as a working refernce to the tutorial articles or a starting point for new projects.
